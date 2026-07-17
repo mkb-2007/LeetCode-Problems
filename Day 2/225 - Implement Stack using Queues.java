c93a761ed394cd0fd75405a392177ca245f78375
Mirza Kareem Baig F <mirzakareembaig2007@outlook.com>
2026-07-17 11:49:31 +0530

    Create 225 - Implement Stack using Queues.java

---
+++ Day 2/225 - Implement Stack using Queues.java
@@ -0,0 +1,29 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class MyStack {
+    private Queue<Integer> queue;
+
+    public MyStack() {
+        queue = new LinkedList<>();
+    }
+    
+    public void push(int x) {
+        queue.add(x);
+        for (int i = 0; i < queue.size() - 1; i++) {
+            queue.add(queue.remove());
+        }
+    }
+    
+    public int pop() {
+        return queue.remove();
+    }
+    
+    public int top() {
+        return queue.peek();
+    }
+    
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}

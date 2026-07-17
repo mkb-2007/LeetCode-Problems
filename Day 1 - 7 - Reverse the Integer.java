ed51de34610e0872440cc8965e6461b25525c510
Mirza Kareem Baig F <mirzakareembaig2007@outlook.com>
2026-07-17 10:37:42 +0530

    Update Day 1 - 7 - Reverse the Integer.java

--- Day 1 - 7 - Reverse the Integer.java
+++ Day 1 - 7 - Reverse the Integer.java
@@ -1,1 +1,18 @@
-
+class Solution {
+    public int reverse(int x) {
+        long reversed = 0;
+        
+        while (x != 0) {
+            int pop = x % 10;
+            reversed = reversed * 10 + pop;
+            x /= 10;
+        }
+        
+        // Check for 32-bit signed integer overflow
+        if (reversed < Integer.MIN_VALUE || reversed > Integer.MAX_VALUE) {
+            return 0;
+        }
+        
+        return (int) reversed;
+    }
+}

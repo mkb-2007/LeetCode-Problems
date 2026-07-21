1d5b24525198ba82fa588248b65714475fd2e5a7
Mirza Kareem Baig F <mirzakareembaig2007@outlook.com>
2026-07-21 10:47:07 +0530

    Delete Day 5/2618 - Check if Object Instance of Class HW.java

--- Day 5/2618 - Check if Object Instance of Class HW.java
+++
@@ -1,7 +0,0 @@
-var checkIfInstanceOf = function(obj, classFunction) {
-    if (obj == null || typeof classFunction !== 'function') {
-        return false;
-    }
-    
-    return Object(obj) instanceof classFunction;
-};

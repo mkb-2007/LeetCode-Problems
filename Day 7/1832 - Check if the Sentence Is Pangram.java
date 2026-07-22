812ab9b874d13a9022b295fb95171a44c4ebb228
Mirza Kareem Baig F <mirzakareembaig2007@outlook.com>
2026-07-22 08:46:04 +0530

    Create 1832 - Check if the Sentence Is Pangram.java

---
+++ Day 7/1832 - Check if the Sentence Is Pangram.java
@@ -0,0 +1,12 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        Set<Character> seen = new HashSet<>();
+        for (char c : sentence.toCharArray()) {
+            seen.add(c);
+        }
+        return seen.size() == 26;
+    }
+}
